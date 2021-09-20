881c6d3d1f6a234b181c3196a92b69ea12a4851e
Samet Kuşbey <sametkusbey36@gmail.com>
2021-09-20 22:13:20 +0300

    Filter function has been developed

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import { useState } from 'react'
 
 function List({ contacts }) {
+
+
+    //filter method
+    const [filterText, setfilterText] = useState("")
+    const filtered = contacts.filter((item) => {  // filtreleme işlemini yaptım.
+        return Object.keys(item).some((key) => {
+           return  item[key]
+                .toString()
+                .toLowerCase().includes(filterText.toLowerCase())
+        })
+    })
     console.log(contacts)
     return (
         <div>
             <div>
-
+                <input placeholder="Filter Contact" value={filterText} onChange={(e) => setfilterText(e.target.value)} />
                 <div>
                     {
-                        contacts.map((contact) => { //contacts geriye array döndüğü için mapledim ve değerleri ekrana bastım
-                            return <div>
-                                <label>Name:</label> {contact.fullname} 
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <label>Phone: </label>
-                                <span>{contact.phone_number}</span>
-                            </div>
+                        filtered.map((contact, index) => { //filterelenen veriyi map ile döndüm ve ekranda kullanıcıya gösterdim.
+                            return <ul>
+                                <li>
+                                    <label>Name:</label>
+                                    <span key={index}> {contact.fullname} </span>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <label>Phone: </label>
+                                    <span key={index}>{contact.phone_number}</span>
+                                </li>
+                            </ul>
                         })
 
                     }</div>
